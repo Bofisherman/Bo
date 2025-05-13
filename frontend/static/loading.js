39ff15d9596914710803aed6bf1a079d8bbb5fee
梁博 <liangbo@BOs-MacBook-Pro.local>
2025-05-13 13:53:16 +0200

    Add custom loading screen

---
+++ frontend/static/loading.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+  document.getElementById("loading-screen").style.display = "none";
+  document.getElementById("main-content").style.display = "flex";
+  document.getElementById("continue").style.display = "flex";
+});
