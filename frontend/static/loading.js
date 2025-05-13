1fcccf2be657d83b09be7861fc33933ff8a5eb0b
梁博 <liangbo@BOs-MacBook-Pro.local>
2025-05-13 15:43:17 +0200

    Add website logo

--- frontend/static/loading.js
+++ frontend/static/loading.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function () {
-  document.getElementById("loading-screen").style.display = "none";
+  document.getElementById("loader").style.display = "none";
+});
   document.getElementById("main-content").style.display = "flex";
   document.getElementById("continue").style.display = "flex";
-});
+
