e0dccea5fde588bf1b05caffe94f0a0791ccf4a6
梁博 <liangbo@BOs-MacBook-Pro.local>
2025-05-14 13:09:20 +0200

    Initial commit with Flask app and site pages

--- app.py
+++ app.py
@@ -7,9 +7,21 @@ app = Flask(__name__,
             template_folder='frontend/templates')
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('home.html')
+
+@app.route('/lessons')
+def lessons():
+    return render_template('lessons.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/support')
+def support():
+    return render_template('support.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # use Render's port if defined
